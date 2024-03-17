42ebe2b51ef1b7c4564fa43ddfb1bed0248d8464
Bruno Albuquerque <bga@gmail.com>
2024-03-17 17:09:29 -0400

    Opportunistic expiration refresh.
    - Also, GET returns a string.

--- internal/lua/lock_script.lua
+++ internal/lua/lock_script.lua
@@ -3,10 +3,12 @@
 -- KEYS[2] - writer count.
 -- ARGV[1] - expiration time in milliseconds.
 
+-- Opportunistically set the expiration time for the writer count key.
+redis.call('PEXPIRE', KEYS[2], ARGV[1])
+
 -- Get reader/writer counts.
 local reader_count = redis.call('GET', KEYS[1])
 local writer_count = redis.call('GET', KEYS[2])
-
 
 if writer_count == false then
     -- There is no active or waiting writer.
@@ -21,13 +23,17 @@ if writer_count == false then
     end
 end
 
+-- GET returns a string. Convert it to a number.
+writer_count = tonumber(writer_count)
+
 -- There is an active or waiting writer.
-if writer_count == 0 then
-    -- There is a waiting writer which might as well be us. Acquire the
-    -- write lock.
+if writer_count == 0 and reader_count == false then
+    -- There is a waiting writer which might as well be us and there are no
+    -- readers. Acquire the write lock.
     redis.call('SET', KEYS[2], 1, 'PX', ARGV[1])
     return 1 -- 1 writer.
 end
     
- -- There is an active writer. We can not acquire the write lock.
+ -- There is an active writer or an intent to write and active readers. We can
+ -- not acquire the write lock.
 return -1
--- internal/lua/rlock_script.lua
+++ internal/lua/rlock_script.lua
@@ -3,18 +3,13 @@
 -- KEYS[2] - writer flag.
 -- ARGV[1] - expiration time in milliseconds.
 
+-- Opportunistically set the expiration time for the reader count key.
+redis.call('PEXPIRE', KEYS[1], ARGV[1])
+
 if redis.call('EXISTS', KEYS[2]) == 1 then
     -- A writer is either waiting or running. We can not have the lock.
     return -1
 end
 
-local reader_result = redis.call('INCR', KEYS[1])
-
-if redis.call('PEXPIRE', KEYS[1], ARGV[1]) == 0 then
-    -- An error happened. Return it.
-    return redis.error_reply(
-        "Failed to set expiration time for reader count key.")
-end
-
--- We acquired the lock.
-return reader_result
+-- Increment the reader count and return the new value.
+return redis.call('INCR', KEYS[1])
