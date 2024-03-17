ed334d16c0ffa4cdc78f97fa547ce2b9f277f6fa
Bruno Albuquerque <bga@gmail.com>
2024-03-17 17:06:38 -0400

    We are going to use opportiunistic refresh.

--- internal/script.go
+++ internal/script.go
@@ -18,9 +18,6 @@ var rlockScript string
 //go:embed lua/runlock_script.lua
 var runlockScript string
 
-//go:embed lua/refresh_script.lua
-var refreshScript string
-
 var (
 	// LockScript is the Lua script for acquiring a lock.
 	LockScript = redis.NewScript(lockScript)
@@ -33,7 +30,4 @@ var (
 
 	// RUnlockScript is the Lua script for releasing a read lock.
 	RUnlockScript = redis.NewScript(runlockScript)
-
-	// RefreshScript is the Lua script for refreshing a lock.
-	RefreshScript = redis.NewScript(refreshScript)
 )
